67e61831b879e2a2187d79e38caddd1a01a46c1e
Brad Treloar <bradley.treloar@gmail.com>
2018-02-22 15:43:31 +1030

    added testGetServices, and added more assertions to testGetRates.

--- tests/StarShipIT/WebServiceClientTest.php
+++ tests/StarShipIT/WebServiceClientTest.php
@@ -22,7 +22,7 @@ class WebServiceClientTest extends TestCase
    * Tests whether client gets a suggestion for an incomplete
    * address that provides the minimum required fields
    */
-  public function testSuggestion() {
+  public function testGetAddressSuggestion() {
     // this address contains the minimum required fields
     // to return a suggestion from the server
     $address = new Address([
@@ -55,7 +55,7 @@ class WebServiceClientTest extends TestCase
    * Tests whether client gets no suggestions an incomplete
    * address that fails to provide a postcode
    */
-  public function testNoSuggestionWithoutPostCode() {
+  public function testGetNoAddressSuggestion() {
     // an address that should fail to return any suggestions
     // since it lacks a postcode
     $address = new Address([
@@ -76,7 +76,7 @@ class WebServiceClientTest extends TestCase
    * Tests whether client gets shipping rates for a valid
    * address and weight
    */
-  public function testRates() {
+  public function testGetRates() {
     // a known valid address
     $address = new Address([
       "street"    => "Parliament Drive",
@@ -88,7 +88,48 @@ class WebServiceClientTest extends TestCase
     ]);
 
     $rates = $this->client->getRates($address, 25.0);
-    // check if an array was returned
+
+    // check that an array was returned
     $this->assertInternalType(PHPUnit_IsType::TYPE_ARRAY, $rates);
+
+    // validate the structure of the rates array
+    if (is_array($rates)) {
+      foreach ($rates as $rate) {
+        $this->assertArrayHasKey('service_name', $rate);
+        $this->assertArrayHasKey('service_code', $rate);
+        $this->assertArrayHasKey('total_price',  $rate);
+      }
+    }
+  }
+
+  /**
+   * Tests whether client gets delivery services for a valid
+   * address and weight
+   */
+  public function testGetServices() {
+    // a known valid address
+    $address = new Address([
+      "street"    => "Parliament Drive",
+      "suburb"    => "Capital Hill",
+      "city"      => "",
+      "state"     => "ACT",
+      "post_code" => "2600",
+      "country"   => "Australia"
+    ]);
+
+    $services = $this->client->getServices($address, 25.0);
+
+    // check if an array was returned
+    $this->assertInternalType(PHPUnit_IsType::TYPE_ARRAY, $services);
+
+    // validate the structure of the services array
+    if (is_array($services)) {
+      foreach ($services as $service) {
+        $this->assertArrayHasKey('carrier',      $service);
+        $this->assertArrayHasKey('service_name', $service);
+        $this->assertArrayHasKey('service_code', $service);
+        $this->assertArrayHasKey('total_price',  $service);
+      }
+    }
   }
 }
